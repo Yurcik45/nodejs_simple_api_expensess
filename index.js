c0c0bd4488dd2de672529dadb6a03d56a1a335e7
Yurcik45 <yurcik45mail@gmail.com>
2023-08-03 17:53:36 +0300

    add primitive crud api

--- index.js
+++ index.js
@@ -8,7 +8,45 @@ app.use(cors())
 
 app.listen(PORT, () => console.log("Server has been started on port ", PORT))
 
+const temp_arr = JSON.stringify([])
+
 app.get('/', (req, res) =>
 {
   res.status(200).json("hello")
 })
+
+const check_type = req =>
+{
+  const type = req.query.typ
+  if (!type) return false
+  return type === "expensess" || type === "benefits"
+}
+
+app.get('/items', (req, res) =>
+{
+  if (!check_type(req)) res.status(400)
+  // request to db, get need items
+  res.status(200).json(temp_arr)
+})
+
+app.post('/items', (req, res) =>
+{
+  if (!check_type(req)) res.status(400)
+  // request to db, post need item
+  res.status(201)
+})
+
+app.update('/items', (req, res) =>
+{
+  if (!check_type(req)) res.status(400)
+  // request to db, update need item
+  res.status(201)
+}
+
+app.delete('/items', (req, res) =>
+{
+  if (!check_type(req)) res.status(400)
+  // request to db, delete need item
+  res.status(200)
+}
+
